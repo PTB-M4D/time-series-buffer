5f9799edbfe05d8c87645593baa174ba70d3fee3
Maximilian Gruber <maximilian.gruber@ptb.de>
2020-06-14 20:06:54 +0200

    feat(TimeSeriesBuffer): implement basic functions

--- time-series-buffer/buffer.py
+++ time-series-buffer/buffer.py
@@ -1,12 +1,20 @@
 from collections import deque
+
 import numpy as np
+from uncertainties import unumpy
 
 
-class TimesSeriesBuffer:
+__all__ = ["TimeSeriesBuffer"]
+
+class TimeSeriesBuffer:
     """
     Custom buffer class, that allows to save streams of time-series with uncertainty 
     in timestamps and values. Acts like a FIFO buffer.
     """
+
+    empty_value = np.nan
+    empty_unc = 0.0  # np.nan?
+    supported_iterable_types = [list, tuple, np.ndarray]
 
     def __init__(self, maxlen=10, return_type="array"):
         """Initialize a FIFO buffer.
@@ -31,7 +39,7 @@ class TimesSeriesBuffer:
     def __len__(self):
         return len(self.buffer)
 
-    def add(self, data=None, time=None, time_unc=None, val=None, val_unc=None):
+    def add(self, data=None, time=self.empty_value, time_unc=self.empty_unc, val=self.empty_value, val_unc=self.empty_unc):
         """Append one or more new datapoints to the buffer. 
         A datapoint consists of the tuple (time, time_uncertainty, value, value_uncertainty).
         
@@ -44,54 +52,97 @@ class TimesSeriesBuffer:
                 * M==3 (triple): assumed to be like (time, value, value_unc)
                 * M==4 (4-tuple): assumed to be like (time, time_unc, value, value_unc)
 
-            time: float, or iterable of float/ufloat (default: None)
+            time: float, or iterable of float/ufloat (default: np.nan)
                 Timestamp(s) to be added.
 
-            time_unc: float, or iterable of float (default: None)
+            time_unc: float, or iterable of float (default: 0.0)
                 Uncertainty(ies) of the timestamp(s) to be added.
 
-            val: (iterable of) float/ufloat (default: None)
+            val: (iterable of) float/ufloat (default: np.nan)
                 Value(s) to be added.
 
-            val_unc: (iterable of) float (default: None)
+            val_unc: (iterable of) float (default: 0.0)
                 Uncertainty(ies) of the value(s) to be added.
     
             time, time_unc, val, val_unc need to be of same shape, but uncertainties can be omitted. 
         
         """
-        # define list of supported iterable types
-        it_types = [list, tuple, np.ndarray]
 
-        # time series is given as rows of tuples
-        if isinstance(data, it_types):
+        # time series is given as iterable of iterables
+        if isinstance(data, self.supported_iterable_types):
 
-            if len(data[0]) == 2:  # pair
-                for (t, v) in data:
-                    self.buffer.append((t, np.nan, v, np.nan))
+            for datapoint in data:
+                t = self.empty_value
+                ut = self.empty_unc
+                v = self.empty_value
+                uv = self.empty_unc
 
-            elif len(data[0]) == 3:  # triple
-                for (t, v, uv) in data:
-                    self.buffer.append((t, np.nan, v, uv))
+                # datapoint is a pair, could be pair of float or pair of ufloat
+                if len(datapoint) == 2:
+                    if isinstance(datapoint[0], uncertainties.core.Variable):
+                        t = datapoint[0].nominal_value
+                        ut = datapoint[0].std_dev
+                    else:
+                        t = datapoint[0]
 
-            elif len(data[0]) == 4:  # 4-tuple
-                for (t, ut, v, uv) in data:
-                    self.buffer.append((t, ut, v, uv))
+                    if isinstance(datapoint[1], uncertainties.core.Variable):
+                        v = datapoint[1].nominal_value
+                        uv = datapoint[1].std_dev
+                    else:
+                        v = datapoint[1]
+
+                # datapoint is a triple
+                elif len(datapoint) == 3:  # triple
+                    t = datapoint[0]
+                    v = datapoint[1]
+                    uv = datapoint[2]
+
+                elif len(datapoint) == 4:  # 4-tuple
+                    t = datapoint[0]
+                    ut = datapoint[1]
+                    v = datapoint[2]
+                    uv = datapoint[3]
+
+                self.buffer.append((t, ut, v, uv))
 
         # time series is given as iterable of floats
-        elif isinstance(time, it_types) and isinstance(val, it_types):
-            if isinstance(unc, it_types):
-                for t, v, u in zip(time, val, unc):
-                    self.buffer.append((t, v, u))
+        elif isinstance(time, self.supported_iterable_types):
+            # time
+            t = time
+
+            # time uncertainty (could be array of same shape as time, single float or None)
+            if isinstance(time_unc, self.supported_iterable_types):
+                ut = time_unc
             else:
-                for t, v in zip(time, val):
-                    self.buffer.append((t, v, np.nan))
+                ut = [time_unc] * len(time)
+
+            # value (could be array of same shape as time, single float or None)
+            if isinstance(val, self.supported_iterable_types):
+                v = val
+            else:
+                v = [val] * len(time)
+
+            # value uncertainty (could be array of same shape as time, single float or None)
+            if isinstance(val_unc, self.supported_iterable_types):
+                uv = val_unc
+            else:
+                uv = [val_unc] * len(time)
+
+            for datapoint in zip(t, ut, v, uv):
+                self.buffer.append(datapoint)
+        
+        elif isinstance(time, float):
+            self.buffer.append((time, time_unc, val, val_unc))
+
+        else:
+            raise ValueError("Your provided type for data or time is not supported.")
 
     def pop(self, n_samples=1):
         """
         Return the next `n_samples` from the left side of the buffer.
 
-        View the latest `n` additions to the buffer. Returns the same format that
-        :py:func:`append_multi` accepts.
+        View the latest `n` additions to the buffer. Returns the format that
+        was specified during init of the buffer.
         
         Parameters
         ----------
@@ -107,13 +158,13 @@ class TimesSeriesBuffer:
         n_pop = min(n_samples, len(self.buffer))
         next_samples = [self.buffer.popleft() for i in range(n_pop)]
 
-        # return as numpy array
-        return np.array(next_samples)
+        # return as specified
+        return self._return_converter(next_samples)
 
     def show(self, n_samples=1):
         """
-        View the latest `n` additions to the buffer. Returns the same format that
-        :py:func:`append_multi` accepts.
+        View the latest `n_samples` additions to the buffer. Returns the format that
+        was specified during init of the buffer.
         """
 
         # take the next samples from the beginning of buffer
@@ -121,5 +172,36 @@ class TimesSeriesBuffer:
         n_pop = min(n_samples, n_buffer)
         next_samples = [self.buffer[n_buffer - (i + 1)] for i in range(n_pop)]
 
-        # return as numpy array
-        return np.vstack(next_samples)
+        # return as specified
+        return self._return_converter(next_samples)
+
+    def _return_converter(self, samples):
+        
+        if self.return_type == "list":
+            return samples
+
+        else:
+            data = np.array(samples)
+            t = data[:,0]
+            ut = data[:,1]
+            v = data[:,2]
+            uv = data[:,3]
+
+            if self.return_type == "array":
+                return np.vstack(data)
+
+            elif self.return_type == "arrays":
+                return t, ut, v, uv
+
+            elif self.return_type == "uarray":
+                tt = unumpy.uarray(t, ut)
+                vv = unumpy.uarray(v, uv)
+
+                return np.vstack(tt,vv).T
+
+            elif self.return_type == "uarrays":
+                tt = unumpy.uarray(t, ut)
+                vv = unumpy.uarray(v, uv)
+
+                return tt, vv
+
